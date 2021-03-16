7f016c1572bbff0c0e0c1814399c6cd61f197473
Maciej <maciej@nagne.studio>
2021-03-16 14:29:12 +0100

    Update bash script

--- run_exp.sh
+++ run_exp.sh
@@ -8,4 +8,6 @@ mv -f experiment.txt results/$2
 zip -r code.zip code
 mv -f code.zip results/$2
 
-# run_experiment -b segmentation_arrayjob.sh -e results/$2/experiment.txt \ --partition=PGR-Standard --cpus-per-task=4 --gres=gpu:1 --mem=20000
+source ~/.bashrc
+
+run_experiment -b segmentation_arrayjob.sh -e results/$2/experiment.txt \ --partition=PGR-Standard --cpus-per-task=4 --gres=gpu:1 --mem=20000
